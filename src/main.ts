2dd65a4c862af23340e128f3eb21d90edaf9d361
RileyChampion <rchamp2u@gmail.com>
2025-02-22 16:36:28 -0500

    Starter code for Nest API

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Adventurer's Journal API")
+    .setDescription("The DND Note Taking app API")
+    .setVersion('1.0')
+    .addTag("adventure")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
